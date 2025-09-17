f2247a0e7ec06e015023441fcf8ec9dfaaad009a
rrsain <ruesrain@gmail.com>
2025-09-17 11:41:46 -0700

    feat(routes): add standard-builder child routes

--- src/containers/standard-builder/dimensions/index.tsx
+++ src/containers/standard-builder/dimensions/index.tsx
@@ -1,4 +1,4 @@
-export default function StandardBuilderPage() {
+export default function Dimensions() {
   return (
     <div className="flex items-center justify-center h-screen bg-white">
       <h1 className="text-3xl font-bold text-black">Standard Builder Page</h1>
