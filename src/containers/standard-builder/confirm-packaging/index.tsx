251b853ea9a9ff6bb1b215cd44894c25ce49f9c5
rrsain <ruesrain@gmail.com>
2025-09-18 11:33:01 -0700

    feat: add a routes of packaging

---
+++ src/containers/standard-builder/confirm-packaging/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ConfirmPackaging = () => {
+  return (
+    <div>
+      ConfirmPackaging
+    </div>
+  )
+}
+
+export default ConfirmPackaging
---
+++ src/containers/standard-builder/packaging/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Packaging = () => {
+  return (
+    <div>
+      Packaging
+    </div>
+  )
+}
+
+export default Packaging
