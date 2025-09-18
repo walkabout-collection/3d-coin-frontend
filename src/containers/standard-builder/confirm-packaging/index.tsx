9704f4e7908574b4be4fb6aaa127f7084358e360
rrsain <ruesrain@gmail.com>
2025-09-18 12:07:44 -0700

    feat:Add note to Packaging component

--- src/containers/standard-builder/confirm-packaging/index.tsx
+++ src/containers/standard-builder/confirm-packaging/index.tsx
@@ -1,11 +1,52 @@
-import React from 'react'
+'use client';
+import Button from '@/src/components/common/button/Button';
+import React from 'react';
+import { useRouter } from 'next/navigation';
 
 const ConfirmPackaging = () => {
-  return (
-    <div>
-      ConfirmPackaging
-    </div>
-  )
-}
+  const router = useRouter();
 
-export default ConfirmPackaging
+  const handleNoClick = () => {
+    router.push('/design-summary');
+  };
+
+  const handleYesClick = () => {
+    router.push('/standard-builder/packaging');
+  };
+
+  return (
+    <div className="min-h-screen">
+      <div className="py-8">
+        <div className="text-center px-4">
+          <h1 className="text-4xl md:text-4xl font-bold text-gray-800 mt-28">
+            Retail Packaging?
+          </h1>
+          <div className="flex flex-col md:flex-row justify-center gap-6 max-w-lg mx-auto mt-14">
+            <Button
+              type="button"
+              variant="ternary"
+              className="!bg-gray-100 border-none font-medium py-6 px-6 rounded-lg hover:border-amber-500 hover:border-2 hover:bg-white hover:shadow-amber-400 hover:shadow-sm text-center"
+              onClick={handleNoClick}
+            >
+              <div className="text-lg leading-tight">
+                No
+              </div>
+            </Button>
+            <Button
+              type="button"
+              variant="ternary"
+              className="!bg-gray-100 border-none font-medium py-5 px-6 rounded-lg hover:border-amber-400 hover:border-2 hover:shadow-amber-400 hover:shadow-sm text-center"
+              onClick={handleYesClick}
+            >
+              <div className="text-lg leading-tight">
+                Yes
+              </div>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmPackaging;
