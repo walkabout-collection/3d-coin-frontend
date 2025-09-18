4ab2a58fbb7f6d7bc2902fd4279332c856b052f8
rrsain <ruesrain@gmail.com>
2025-09-18 06:53:35 -0700

    feat(standard-builder): add edge type  page

---
+++ src/containers/standard-builder/edge-type/data.ts
@@ -0,0 +1,32 @@
+export const edgeTypeOptions = [
+  {
+    id: 'smooth',
+    name: 'Smooth',
+    image: '/images/standard/smooth.svg',
+  },
+  {
+    id: 'rope',
+    name: 'Rope',
+    image: '/images/standard/rope.svg',
+  },
+  {
+    id: 'oblique',
+    name: 'Oblique',
+    image: '/images/standard/oblique.svg',
+  },
+  {
+    id: 'diamond',
+    name: 'Diamond',
+    image: '/images/standard/diamond.svg',
+  },
+  {
+    id: 'grid-pattern',
+    name: 'Grid Pattern',
+    image: '/images/standard/grid-pattern.svg',
+  },
+  {
+    id: 'curve-wave',
+    name: 'Curve Wave',
+    image: '/images/standard/curve-wave.svg',
+  },
+];
