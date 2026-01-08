cf7b7501b287622caa32cc233523db519a8fd190
RRSain <ruesrain@gmail.com>
2026-01-08 09:51:50 -0800

    fix: fixed warnings

--- src/containers/admin/payment-history/types.ts
+++ src/containers/admin/payment-history/types.ts
@@ -1,9 +1,11 @@
 export interface PaymentDataItem {
   paymentMethod: string;
-  orderTotal: string;
-  date: string;
   orderId: string;
-  customer: string;
-  customerEmail: string;
-  status: string;
+  date: string;
+
+  orderTotal?: string;
+  order?: string;
+  customer?: string;
+  customerEmail?: string;
+  status?: string;
 }
